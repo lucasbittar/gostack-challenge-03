dc91618e4cdd113fa20a6df5b40de4f77a03d80b
Lucas Bittar Magnani <lucasbittar.magnani@gmail.com>
2020-02-10 18:48:43 -0300

    Feature: upload avatar

--- src/database/index.js
+++ src/database/index.js
@@ -1,13 +1,14 @@
 import Sequelize from 'sequelize';
 import databaseConfig from '../config/database';
 
-import User from '../app/models/User';
+import File from '../app/models/File';
 import Recipient from '../app/models/Recipient';
+import User from '../app/models/User';
 
 /*
  * Always make sure to populate models array when another table is created
  */
-const models = [User, Recipient];
+const models = [File, Recipient, User];
 
 class Database {
   constructor() {
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,16 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import RecipientController from './app/controllers/RecipientController';
 import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
+import UploadController from './app/controllers/UploadController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
@@ -18,5 +22,7 @@ routes.put('/users', UserController.update);
 routes.post('/recipients', RecipientController.store);
 routes.put('/recipients/:id', RecipientController.update);
 routes.get('/recipients', RecipientController.index);
+
+routes.post('/upload', upload.single('file'), UploadController.store);
 
 export default routes;
