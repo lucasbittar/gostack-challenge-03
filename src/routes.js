bb4eef8cdf8551c3277a2fdebcce5530d0552521
Lucas Bittar Magnani <lucasbittar.magnani@gmail.com>
2020-03-26 17:26:21 -0300

    Add test route to trigger Buddy

--- src/routes.js
+++ src/routes.js
@@ -20,6 +20,8 @@ const upload = multer(multerConfig);
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
+routes.get('/', (req, res) => res.send('ok'));
+
 routes.use(authMiddleware);
 
 /* All routes below requires a valid session */
